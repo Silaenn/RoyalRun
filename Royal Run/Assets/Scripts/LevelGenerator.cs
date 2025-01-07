ad0b78c325bebee3fa3617a6ee8d5d8d7baf8b9f
Deo silaen <deokeldisilaen@gmail.com>
2025-01-07 17:42:08 +0700

    ChunkMovement

--- Royal Run/Assets/Scripts/LevelGenerator.cs
+++ Royal Run/Assets/Scripts/LevelGenerator.cs
@@ -6,24 +6,50 @@ public class LevelGeneretor : MonoBehaviour
     [SerializeField] int startingChunksAmount = 12;
     [SerializeField] Transform chunckParent;
     [SerializeField] float chunkLength = 10f;
+    [SerializeField] float moveSpeed = 8f;
+    GameObject[] chunks = new GameObject[12];
 
+    void Start()
+    {
+        SpawnChunks();
+    }
 
-    void Start() {
+    void Update(){
+        MoveChunks();
+    }
+
+    void SpawnChunks()
+    {
         for (int i = 0; i < startingChunksAmount; i++)
         {
-            float spawnPositionZ;
-
-            if (i == 0) {
-               spawnPositionZ = transform.position.z; 
-            } else {
-                spawnPositionZ = transform.position.z + (i * chunkLength);
-            }
+            float spawnPositionZ = CalculateSpawnPositionZ(i);
             Vector3 chunkSpawnPos = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
-            Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunckParent);
+            GameObject newChunk = Instantiate(chunkPrefab, chunkSpawnPos, Quaternion.identity, chunckParent);
+
+            chunks[i] = newChunk;  
         }
     }
 
+    float CalculateSpawnPositionZ(int i)
+    {
+        float spawnPositionZ;
 
+        if (i == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            spawnPositionZ = transform.position.z + (i * chunkLength);
+        }
 
+        return spawnPositionZ;
+    }
 
+    void MoveChunks() {
+        for (int i = 0; i < chunks.Length; i++)
+        {
+           chunks[i].transform.Translate(-transform.forward * (moveSpeed * Time.deltaTime));
+        }
+    }
 }
