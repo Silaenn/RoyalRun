0f46e059d3ae76818aa37c0270f64d39d6ffa48a
Deo silaen <deokeldisilaen@gmail.com>
2025-01-08 18:22:38 +0700

    ObstaclePrefab

--- Royal Run/Assets/Scripts/Obstacle Spawner.cs
+++ Royal Run/Assets/Scripts/Obstacle Spawner.cs
@@ -1,10 +1,12 @@
-using System.Collections;
+ using System.Collections;
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] GameObject[] obstaclePrefabs;
     [SerializeField] float obstaclesSpawnTime = 1f;
+    [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth = 4f;
 
     void Start() {
         StartCoroutine(SpawnObstacles());
@@ -13,8 +15,10 @@ public class ObstacleSpawner : MonoBehaviour
     IEnumerator SpawnObstacles(){
          while (true)
     {
+        GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z);
         yield return new WaitForSeconds(obstaclesSpawnTime);
-        Instantiate(obstaclePrefab, transform.position, Random.rotation);
+        Instantiate(obstaclePrefab, spawnPosition, Random.rotation, obstacleParent);
       }
    }
     
