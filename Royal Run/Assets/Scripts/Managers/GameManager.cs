25968f7bc6bf3ea4eafa9a79428dd33a6564e75c
Deo silaen <deokeldisilaen@gmail.com>
2025-01-10 00:39:23 +0700

    addTimeLose

---
+++ Royal Run/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] PlayerController playerController;
+    [SerializeField] TMP_Text timeText;
+    [SerializeField] GameObject gameOverText;
+    [SerializeField] float startTime = 5f;
+
+    float timeLeft;
+    bool gameOver = false;
+
+    void Start() {
+        timeLeft = startTime;     
+    }
+
+    void Update()
+    {
+        DecreaseTime();
+    }
+
+    private void DecreaseTime()
+    {
+        if (gameOver) return;
+
+        timeLeft -= Time.deltaTime;
+        timeText.text = timeLeft.ToString("F1");
+
+        if (timeLeft <= 0f)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        gameOver = true;
+        playerController.enabled = false;
+        gameOverText.SetActive(true);
+        Time.timeScale = .1f;
+    }
+}
